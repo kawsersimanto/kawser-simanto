4473cc9d4cccc78e3340f6452ba3a863f5f19246
simu6 <semantos678@gmail.com>
2022-09-18 21:06:38 +0600

    completed service portfolio and counter section

--- js/script.js
+++ js/script.js
@@ -17,3 +17,17 @@ home.addEventListener("mousemove", (e) => {
   cursor.style.left = e.pageX - 40 + "px";
   cursor.style.top = e.pageY - 40 + "px";
 });
+
+// init Isotope
+var $portfolioList = $(".portfolio-list").isotope();
+// filter items on button click
+$(".filter-button-group").on("click", "button", function () {
+  var filterValue = $(this).attr("data-filter");
+  $portfolioList.isotope({ filter: filterValue });
+});
+
+// counter
+$(".counter").counterUp({
+  delay: 10,
+  time: 1000,
+});
